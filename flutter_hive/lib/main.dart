c73ea642ec7806dc65dfad93c1a37eefee1ef8b9
Fikri Prayoga <fikriprayoga007@gmail.com>
2021-07-27 23:09:53 +0700

    Update for using in mobile

--- flutter_hive/lib/main.dart
+++ flutter_hive/lib/main.dart
@@ -4,15 +4,28 @@ import 'package:flutter_hive/view/view_home.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'model/model_list_users.dart';
+import 'package:path_provider/path_provider.dart' as pathProvider;
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 // run 'flutter packages pub run build_runner build --delete-conflicting-outputs' in your cmd to generate [file-name].g.dart
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Hive
-    ..initFlutter()
-    ..registerAdapter(ModelDataAdapter())
-    ..registerAdapter(ModelListUsersAdapter());
+  if (kIsWeb) {
+    // running on the web!
+    Hive
+      ..initFlutter()
+      ..registerAdapter(ModelDataAdapter())
+      ..registerAdapter(ModelListUsersAdapter());
+  } else {
+    // NOT running on the web! You can check for additional platforms here.
+    var appDocumentDirectory =
+        await pathProvider.getApplicationDocumentsDirectory();
+    Hive
+      ..init(appDocumentDirectory.path)
+      ..registerAdapter(ModelDataAdapter())
+      ..registerAdapter(ModelListUsersAdapter());
+  }
 
   await initFunc();
 
